9e1f56dd131d5ae93ed7af957877c0941246d79b
Edis Muminovic <nphochbaugmbh@Mac.fritz.box>
2026-03-16 10:30:15 +0100

    fix: blog modal positioning

--- src/app/components/portfolio/Blog.tsx
+++ src/app/components/portfolio/Blog.tsx
@@ -80,7 +80,7 @@ export const Blog = ({ language }: BlogProps) => {
 
             <AnimatePresence>
                 {selectedPost && (
-                    <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
+                    <div className="fixed inset-0 z-[100] flex items-center justify-center p-4 md:p-8">
                         <motion.div 
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
